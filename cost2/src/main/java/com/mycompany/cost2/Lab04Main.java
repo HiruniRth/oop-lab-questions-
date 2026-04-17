7d626d525855a71a173fe347306a3793f8959f8e
HiruniRth <04rathnayake@gmail.com>
2026-04-17 20:42:49 +0530

    lab04 q4 final policy class

--- cost2/src/main/java/com/mycompany/cost2/Lab04Main.java
+++ cost2/src/main/java/com/mycompany/cost2/Lab04Main.java
@@ -1,18 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.cost2;
 
 public class Lab04Main {
 
     public static void main(String[] args) {
 
-        Lecturer lec = new Lecturer("Nimal Perera", "L001", "IT", 3, 50000);
-        LabAssistant lab = new LabAssistant("K. Rathnayake", "A001", "IT", 40, 2000);
+        Lecturer lec = new Lecturer("Nimal", "L001", "IT", 3, 50000);
+        LabAssistant lab = new LabAssistant("Kamal", "A001", "IT", 40, 2000);
 
-    lec.displayLecturerDetails();
-    lab.displayLabAssistantDetails();
+        UniversityPolicy.showPolicyHeader();
+        System.out.println("University: " + UniversityPolicy.UNIVERSITY_NAME);
+
+        System.out.println();
+        double lecturerBonus = lec.calculateMonthlyPayment();
+        double assistantBonus = lab.calculateMonthlyPayment();
+
+        System.out.println("Lecturer Bonus: " + UniversityPolicy.calculateBonus(lecturerBonus));
+        System.out.println("Lab Assistant Bonus: " + UniversityPolicy.calculateBonus(assistantBonus));
+    }
 }
-}
+
 
