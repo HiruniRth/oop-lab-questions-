d2154a5a37035733539cbdab0759306841b52299
HiruniRth <04rathnayake@gmail.com>
2026-04-17 19:19:51 +0530

    lab04 q1 abstract parent created

---
+++ cost2/src/main/java/lab4/StaffMember.java
@@ -0,0 +1,36 @@
+package lab4;
+public abstract class StaffMember {
+
+    private String fullName;
+    private final String staffId;
+    protected String department;
+
+    public StaffMember(String fullName, String staffId, String department) {
+        this.fullName = fullName;
+        this.staffId = staffId;
+        this.department = department;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public final void displayBasicDetails() {
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Staff ID: " + staffId);
+        System.out.println("Department: " + department);
+    }
+
+    public abstract double calculateMonthlyPayment();
+
+    // StaffMember is abstract because it acts as a general template for all staff types
+    // and should not be used to create objects directly.
+}
