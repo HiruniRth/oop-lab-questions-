e90d8a92e70194eece3a1743c0dfbe548bc3f3dd
HiruniRth <04rathnayake@gmail.com>
2026-04-17 20:09:50 +0530

    lab04 q2 lecturer child class

---
+++ cost2/src/main/java/com/mycompany/cost2/LabAssistant.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cost2;
+
+public class LabAssistant extends StaffMember {
+
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public LabAssistant(String fullName, String staffId, String department,
+                        int hoursWorked, double hourlyRate) {
+
+        super(fullName, staffId, department);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public double calculateMonthlyPayment() {
+        return hoursWorked * hourlyRate;
+    }
+
+    public void displayLabAssistantDetails() {
+        displayBasicDetails();
+        System.out.println("Hours Worked: " + hoursWorked);
+        System.out.println("Hourly Rate: " + hourlyRate);
+    }
+
+    // department is protected so child classes can access it directly
+}
---
+++ cost2/src/main/java/com/mycompany/cost2/UniversityPolicy.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cost2;
+public final class UniversityPolicy {
+
+    public static final String UNIVERSITY_NAME = "Kelaniya University";
+    public static final double BONUS_RATE = 0.1;
+
+    public static void showPolicyHeader() {
+        System.out.println(" University Policy ");
+    }
+
+    public static double calculateBonus(double monthlyPayment) {
+        return monthlyPayment * BONUS_RATE;
+    }
+
+    // final class cannot be inherited to protect rules
+}
