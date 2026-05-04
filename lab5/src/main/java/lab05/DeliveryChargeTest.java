d98181ca070c59c1a40dc4b57ce0d531f4642b7b
HiruniRth <04rathnayake@gmail.com>
2026-05-04 14:18:20 +0530

    lab 5 q1 DeliveryChargeTest class

---
+++ lab5/src/main/java/lab05/DeliveryChargeTest.java
@@ -0,0 +1,21 @@
+
+package lab05;
+
+public class DeliveryChargeTest {
+    public static void main(String[] args) {
+
+        DeliveryChargeCalculator calculator = new DeliveryChargeCalculator();
+
+        double charge1 = calculator.calculateCharge(1000);
+        double charge2 = calculator.calculateCharge(1000, 5);
+        double charge3 = calculator.calculateCharge(1000, 5, 2);
+        double charge4 = calculator.calculateCharge(1000, true);
+
+        System.out.println("Base Charge: " + charge1);
+        System.out.println("With Distance: " + charge2);
+        System.out.println("With Distance & Weight: " + charge3);
+        System.out.println("With Express Delivery: " + charge4);
+    }
+    // This is compile-time polymorphism because method overloading is resolved at compile time.
+}
+    
