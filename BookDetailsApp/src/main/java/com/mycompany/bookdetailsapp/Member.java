d75eff6fc1d4cc3fdde0ec21250417d079574516
HiruniRth <04rathnayake@gmail.com>
2026-05-24 20:40:19 +0530

    lab 6 q4

---
+++ BookDetailsApp/src/main/java/com/mycompany/bookdetailsapp/Member.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bookdetailsapp;
+
+/**
+ *
+ * @author Dell
+ */
+public class Member {
+    private String memberName;
+    private String membershipType;
+    private boolean newsletterSelected;
+    private int numberOfMonths;
+    
+    public Member(String memberName,String membershipType,boolean newsletterSelected,int numberOfMonths){
+        this.memberName = memberName ;
+        this.membershipType = membershipType;
+        this.newsletterSelected = newsletterSelected;
+        this.numberOfMonths = numberOfMonths;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public boolean isNewsletterSelected() {
+        return newsletterSelected;
+    }
+
+    public int getNumberOfMonths() {
+        return numberOfMonths;
+    }
+
+    public double getMonthlyFee(){
+        if (membershipType.equals("Regular"))
+            return 1000;
+        else 
+            return 2500;       
+    }
+     
+    public double getMembershipFee(){       
+        double totalFee = getMonthlyFee() * numberOfMonths;
+        
+        if (newsletterSelected)
+            totalFee = totalFee + 200; 
+        return totalFee;
+    }                   
+               
+    public String getNewsletterText(){
+        if(newsletterSelected)
+            return "Yes";
+
+        else
+            return "No";
+    }
+}    
+
+        
+     
+
